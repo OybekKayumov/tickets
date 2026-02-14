4360754ec9d2c03cc42ee9414f9567e8dfd24c68
Oybek Kayumov <oybek.kayumov@gmail.com>
2026-02-14 10:21:05 +0500

    Add Published Event search

--- src/main/java/com/ok/tickets/controller/PublishedEventController.java
+++ src/main/java/com/ok/tickets/controller/PublishedEventController.java
@@ -1,6 +1,7 @@
 package com.ok.tickets.controller;
 
 import com.ok.tickets.domain.dto.ListPublishedEventResponseDto;
+import com.ok.tickets.domain.enteties.Event;
 import com.ok.tickets.mappers.EventMapper;
 import com.ok.tickets.services.EventService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,9 +20,21 @@ public class PublishedEventController {
 	private final EventService eventService;
 	private final EventMapper eventMapper;
 
-	public ResponseEntity<Page<ListPublishedEventResponseDto>> listPublishedEvents(Pageable pageable) {
+	public ResponseEntity<Page<ListPublishedEventResponseDto>> listPublishedEvents(
+					@RequestParam(required = false) String q,
+					Pageable pageable) {
 
-		return ResponseEntity.ok(eventService.listPublishedEvents(pageable)
-						.map(eventMapper::toListPublishedEventResponseDto));
+		Page<Event> events;
+
+		if (q != null && !q.trim().isEmpty()) {
+
+			events= eventService.searchPublishedEvents(q, pageable);
+		} else {
+			events = eventService.listPublishedEvents(pageable);
+		}
+
+		return ResponseEntity.ok(
+						events.map(eventMapper::toListPublishedEventResponseDto));
 	}
+
 }
