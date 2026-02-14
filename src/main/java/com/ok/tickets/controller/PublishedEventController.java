e311e2c5299857d42b05cb5bcc18f5a974405fa3
Oybek Kayumov <oybek.kayumov@gmail.com>
2026-02-14 09:41:48 +0500

    Add Published Event controller

---
+++ src/main/java/com/ok/tickets/controller/PublishedEventController.java
@@ -0,0 +1,26 @@
+package com.ok.tickets.controller;
+
+import com.ok.tickets.domain.dto.ListPublishedEventResponseDto;
+import com.ok.tickets.mappers.EventMapper;
+import com.ok.tickets.services.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/published-events")
+@RequiredArgsConstructor
+public class PublishedEventController {
+
+	private final EventService eventService;
+	private final EventMapper eventMapper;
+
+	public ResponseEntity<Page<ListPublishedEventResponseDto>> listPublishedEvents(Pageable pageable) {
+
+		return ResponseEntity.ok(eventService.listPublishedEvents(pageable)
+						.map(eventMapper::toListPublishedEventResponseDto));
+	}
+}
